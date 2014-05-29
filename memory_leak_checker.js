7fd4abf8fe985b5f45e3b45194a30a498aa80d28
Guilherme Nascimento <new_guil1503@hotmail.com>
2014-05-29 14:16:29 -0300

    Released code
    Added "use strict";
    jshint and jslint is ok (facilitate developers do updates)
    `uniq_id` to string, so can compare with "===" instead of "=="
    Tests for possible errors in (try/catch) `obj.x_leaks_checked`

--- memory_leak_checker.js
+++ memory_leak_checker.js
@@ -1,85 +1,107 @@
-MemoryLeakChecker = {
+(function (window) {
+    "use strict";
 
-    uniq_id: (new Date()).getTime(),
-    checked: 1,
-    is_seen: [],
+    var MemoryLeakChecker = {
+        "uniq_id": String((new Date()).getTime()),
+        "checked": 1,
+        "is_seen": [],
 
-    checkLeaks: function(obj) {
-        var self = MemoryLeakChecker
+        "log": function () {
+            if (window.console && window.console.log) {
+                window.console.log.apply(window.console, arguments);
+            }
+        },
 
-        if(!obj || (typeof obj == 'function') || self.checked > 20000)
-            return ;
+        "checkLeaks": function (obj) {
+            var self = MemoryLeakChecker, key, i, j;
 
-        if((self._isArray(obj) || self._isObject(obj))) {
-            if(self._isArray(obj)) {
-                self._logTooBig(obj, obj.length)
-                for(var i=0; i < obj.length; i++) {
-                    self._checkIfNeeded(obj[i])
+            if (!obj || (typeof obj === "function") || self.checked > 20000) {
+                return;
+            }
+
+            if (self.isArray(obj) || self.isObject(obj)) {
+                if (self.isArray(obj)) {
+                    self.logTooBig(obj, obj.length);
+                    j = obj.length;
+                    for (i = 0; i < j; i += 1) {
+                        self.checkIfNeeded(obj[i]);
+                    }
+                } else if (self.isObject(obj)) {
+                    self.logTooBig(obj, self.getKeys(obj).length);
+
+                    for (key in obj) {
+                        if (obj.hasOwnProperty(key)) {
+                            self.checkIfNeeded(obj[key]);
+                        }
+                    }
                 }
             }
-            else if(self._isObject(obj)) {
-                self._logTooBig(obj, self._keys(obj).length)
+        },
 
-                for(var key in obj) {
-                    self._checkIfNeeded(obj[key])
+        "checkIfNeeded": function (obj) {
+            if (!obj) {
+                return;
+            }
+
+            var self = MemoryLeakChecker;
+            self.checked += 1;
+
+            if (self.isArray(obj) || self.isObject(obj)) {
+                if (obj.x_leaks_checked === self.uniq_id) {
+                    return;
+                }
+
+                try {
+                    obj.x_leaks_checked = self.uniq_id;
+                } catch (ee) {
+                     self.log(obj, ee);
+                }
+
+                setTimeout(self.partial(self.checkLeaks, obj), 5);
+            }
+        },
+
+        "logTooBig": function (obj, limit) {
+            if (limit > 200) {
+                MemoryLeakChecker.log("Object too big, memory leak? [size: " + limit + "]");
+                MemoryLeakChecker.log(obj);
+                MemoryLeakChecker.log("-------");
+            }
+        },
+
+        "getKeys": function (obj) {
+            var rval = [], prop;
+            for (prop in obj) {
+                if (obj.hasOwnProperty(prop)) {
+                    rval.push(prop);
                 }
             }
+            return rval;
+        },
+
+        "isArray": function (obj) {
+            try {
+                return obj instanceof Array;
+            } catch (e) {
+                return false;
+            }
+        },
+
+        "isObject": function (obj) {
+            return (typeof obj === "object");
+        },
+
+        "partial": function (fn) {
+            var args = Array.prototype.slice.call(arguments);
+            args.shift();
+            return function () {
+                var new_args = Array.prototype.slice.call(arguments);
+                args = args.concat(new_args);
+                return fn.apply(window, args);
+            };
         }
-    },
+    };
 
-    _checkIfNeeded: function(obj) {
-        if(!obj)
-            return ;
-
-        var self = MemoryLeakChecker;
-        self.checked++
-
-        if((self._isArray(obj) || self._isObject(obj))) {
-            if(obj.__leaks_checked == self.uniq_id)
-                return ;
-            obj.__leaks_checked = self.uniq_id
-
-            setTimeout(self._partial(self.checkLeaks, obj), 5);
-        }
-    },
-
-    _logTooBig: function(obj, limit) {
-        if(limit > 200) {
-            console.log('Object too big, memory leak? [size: ' + limit + ']')
-            console.log(obj)
-            console.log('-------')
-        }
-    },
-
-    _keys: function(obj) {
-        var rval = [], prop
-        for(prop in obj)
-            rval.push(prop)
-        return rval
-    },
-
-    _isArray: function(obj) {
-        try {
-            return obj instanceof Array
-        }
-        catch(e) {
-            return false
-        }
-    },
-
-    _isObject: function(obj) {
-        return (typeof obj == 'object')
-    },
-
-    _partial: function(fn) {
-        var args = Array.prototype.slice.call(arguments)
-        args.shift()
-        return function() {
-            var new_args = Array.prototype.slice.call(arguments)
-            args = args.concat(new_args)
-            return fn.apply(window, args)
-        }
-    }
-}
-
-MemoryLeakChecker.checkLeaks(window);
+    window.MemoryLeakChecker = MemoryLeakChecker;
+    MemoryLeakChecker.checkLeaks(window);
+}(window));
